a2e942862f73ff71d61e1ec5a39a2d0ef55d471e
Inf1n8 <vikrame1999@gmail.com>
2018-08-21 17:12:25 +0530

    assets included navbar completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
 import $ from 'jquery';
 import 'hammerjs';
 import 'materialize-css/dist/js/materialize';
+
+$( document ).ready(function(){
+    $(".button-collapse").sideNav();
+});
+console.log('loaded');
